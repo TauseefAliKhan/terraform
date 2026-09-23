4ccd3f8f4f7b87cd59f0facbb2713d7315ec30a2
TauseefAliKhan <zai.tausif@gmail.com>
2026-09-23 11:55:18 +0530

    Terraform Devops Lab with variables

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Resource group
 resource "azurerm_resource_group" "devops_terraform_rg" {
   name     = "devops-terraform-rg"
-  location = "South India"
+  location = var.location
 }
 
 
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "location" {
+  description = "Azure region where resources will be deployed"
+  type        = string
+  default     = "South India"
+}
+
+variable "vm_size" {
+  description = "Azure VM size"
+  type        = string
+  default     = "Standard_DS1_v2"
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the VNet"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnets" {
+  description = "Subnets to be created inside the VNet"
+  type        = map(string)
+  default = {
+    management  = "10.0.0.0/24"
+    application = "10.0.1.0/24"
+    database    = "10.0.2.0/24"
+  }
+}
